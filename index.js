6eaae026a5a2c80157a9b4defdacbed575954cc8
Damicela Peña <damicelapr@gmail.com>
2022-01-10 10:55:46 -0600

    10/01/22

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 esta es mi primera linea creada desde master
 linea creada desde my primera branch
 esta es la linea añadidad desde branch 2
+hoy voy a comprobar los conocimientos adquiridos la semana pasada. esta linea esta creada desde my first branch el 10/01/22
+esta linea esta creada desde la second branch el 10/01/22
